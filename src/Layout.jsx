5db7e5d3414635cf152e9521efc3b17b77d5f4a4
Daniyal <daniyalgothacked@gmail.com>
2024-11-18 00:26:41 +0500

    "Updated Layout, CustomCursor, HeroContactBtn, Navbar, and index CSS files with various styling and functionality changes."

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+// import { useEffect } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Outlet } from 'react-router-dom'
 import Footer from './components/footer/Footer'
@@ -6,6 +7,27 @@ import './App.css'
 import CustomCursor from './components/customCursor/CustomCursor'
 
 const Layout = () => {
+
+  // useEffect(() => {
+  //   const scrollSpeed = 4; // Adjust this value for slower/faster scrolling
+
+  //   const handleScroll = (event) => {
+  //     event.preventDefault();
+  //     const delta = event.deltaY * scrollSpeed;
+  //     window.scrollBy({
+  //       top: delta,
+  //       behavior: 'smooth',
+  //     });
+  //   };
+
+  //   window.addEventListener('wheel', handleScroll, { passive: false });
+
+  //   // Cleanup on unmount
+  //   return () => {
+  //     window.removeEventListener('wheel', handleScroll);
+  //   };
+  // }, []);
+
   return (
     <>
     <CustomCursor>
