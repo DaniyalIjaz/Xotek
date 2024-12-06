d0e0f3171ab9cb8d93f7a8d195dcfd0a38941403
Daniyal <daniyalgothacked@gmail.com>
2024-12-06 21:44:53 +0500

    navbar updates

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 // import { useEffect } from 'react'
-import Navbar from './components/Navbar/Navbar'
 import { Outlet } from 'react-router-dom'
 import Footer from './components/footer/Footer'
 import './App.css'
 import CustomCursor from './components/customCursor/CustomCursor'
 import ScrollToTop from './components/scrollToTop/ScrollToTop'
+import Navbar from './components/navbar'
 
 const Layout = () => (
   <>
     <CustomCursor>
       <ScrollToTop />
-      <Navbar />
+      <Navbar/>
       <Outlet />
       <Footer />
     </CustomCursor>
